0991ef85a30724e2919e72fcb5bb934b9fd24ace
Tom van Tilburg <tom.van.tilburg@gmail.com>
2015-12-16 12:55:13 +0100

    flups

--- cowconfigs.js
+++ cowconfigs.js
@@ -9,9 +9,9 @@ var cowconfig_geodan ={
 var cowconfig_local ={
 	key: 'imagine',
 	protocol:'ws',
-	url:'192.168.26.74', 
+	url:'192.168.25.134', 
 	port:8081
   };
 
 //Set the correct config here
-window.cowconfig = cowconfig_geodan;
+window.cowconfig = cowconfig_local;
--- server/httpserver.js
+++ server/httpserver.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 
-app.use('/', express.static(__dirname + '/public'));
-
+//app.use('/', express.static(__dirname + '/public'));
+app.use('/',express.static(__dirname + '/..'));
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
